deff5e29eab94a3f489e78dcde2c3c0e1e8288be
coderick14 <bhattacharyya.rick14@gmail.com>
2017-03-25 16:05:54 +0530

    Add ALU module

---
+++ ALU/ALU.go
@@ -0,0 +1,32 @@
+package ALU
+
+/*
+ * Function to perform signed addition
+ */
+func adder(val1, val2 int64) int64 {
+	return val1 + val2
+}
+
+/*
+ * Function to perform logical AND operation
+ */
+
+func logicalAND(val1, val2 int64) int64 {
+	return val1 & val2
+}
+
+/*
+ * Function to perform logical OR operation
+ */
+
+func logicalOR(val1, val2 int64) int64 {
+	return val1 | val2
+}
+
+/*
+ * Function to perform logical XOR operation
+ */
+
+func logicalXOR(val1, val2 int64) int64 {
+	return val1 ^ val2
+}
