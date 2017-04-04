ed686c2c2025d7f12bf62f0868c3d1a07f6301a9
coderick14 <bhattacharyya.rick14@gmail.com>
2017-04-04 17:59:32 +0530

    Add flag variables and implement word-based memory

--- Memory/DataMemory.go
+++ Memory/DataMemory.go
@@ -4,17 +4,19 @@ import "sync"
 
 type DataMemory struct {
 	sync.RWMutex
-	Memory []int64
+	Memory []int32
 }
 
 var Registers [32]int64
+
+var flagNegative, flagZero, flagOverflow, flagCarry bool
 
 /*
  * Method to read data from memory
  * Guarantees mutually exclusive access
  */
 
-func (dataMemory *DataMemory) read(address uint64) int64 {
+func (dataMemory *DataMemory) read(address uint64) int32 {
 	dataMemory.RLock()
 	value := dataMemory.Memory[address]
 	dataMemory.RUnlock()
@@ -26,7 +28,7 @@ func (dataMemory *DataMemory) read(address uint64) int64 {
  * Guarantees mutually exclusive access
  */
 
-func (dataMemory *DataMemory) write(address uint64, value int64) {
+func (dataMemory *DataMemory) write(address uint64, value int32) {
 	dataMemory.Lock()
 	dataMemory.Memory[address] = value
 	dataMemory.Unlock()
